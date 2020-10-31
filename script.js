42b17e4f6741e25f036ea15a593bf0d06894309f
Forrest Satterfield <forrest@satterfieldtechnologies.com>
2020-10-30 19:43:25 -0500

    hit

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
-// window.onload = () => {
-//     let places = staticLoadPlaces();
-//     renderPlaces(places);
-// };
+window.onload = () => {
+    let places = staticLoadPlaces();
+    renderPlaces(places);
+};
 
-// function staticLoadPlaces() {
-//    return [
-//        {
-//            name: 'Magnemite',
-//            location: {
-//                lat: 33.511929,
-//                lng: -86.812299,
-//            }
-//        },
-//    ];
-// }
+function staticLoadPlaces() {
+   return [
+       {
+           name: 'Magnemite',
+           location: {
+               lat: 33.511929,
+               lng: -86.812299,
+           }
+       },
+   ];
+}
 
-// function renderPlaces(places) {
-//    let scene = document.querySelector('a-scene');
+function renderPlaces(places) {
+   let scene = document.querySelector('a-scene');
 
-//    places.forEach((place) => {
-//        let latitude = place.location.lat;
-//        let longitude = place.location.lng;
+   places.forEach((place) => {
+       let latitude = place.location.lat;
+       let longitude = place.location.lng;
 
-//        let model = document.createElement('a-entity');
-//        model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-//        model.setAttribute('gltf-model', './assets/magnemite/scene.gltf');
-//        model.setAttribute('rotation', '0 180 0');
-//        model.setAttribute('animation-mixer', '');
-//        model.setAttribute('scale', '0.5 0.5 0.5');
+       let model = document.createElement('a-entity');
+       model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+       model.setAttribute('gltf-model', './assets/magnemite/scene.gltf');
+       model.setAttribute('rotation', '0 180 0');
+       model.setAttribute('animation-mixer', '');
+       model.setAttribute('scale', '0.5 0.5 0.5');
 
-//        model.addEventListener('loaded', () => {
-//            window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
-//        });
+       model.addEventListener('loaded', () => {
+           window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+       });
 
-//        scene.appendChild(model);
-//    });
-// }
+       scene.appendChild(model);
+   });
+}
