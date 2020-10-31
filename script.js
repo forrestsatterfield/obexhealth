5ba49cb635a81ecb0f8d5e722e0762b9b8f48c48
Forrest Satterfield <forrest@satterfieldtechnologies.com>
2020-10-30 19:57:26 -0500

    u

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ function renderPlaces(places) {
        model.setAttribute('gltf-model', './assets/magnemite/scene.gltf');
        model.setAttribute('rotation', '0 180 0');
        model.setAttribute('animation-mixer', '');
-       model.setAttribute('scale', '50 50 50');
+       model.setAttribute('scale', '0.5 0.5 0.5');
 
        model.addEventListener('loaded', () => {
            window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
